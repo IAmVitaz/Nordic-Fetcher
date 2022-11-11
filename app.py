1a7861a910293de37d96e5976a236dabd6b43f40
vit_az <vit.azarov@gmail.com>
2022-11-10 21:27:43 -0400

    - Added timestamp.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from timefetcher import TimeFetcher
-
+from datetime import datetime, timedelta, date\
 
 app = Flask(__name__)
 
@@ -11,8 +11,10 @@ def index():
     availableSpots = fetcher.getAvailableTimeSlots()
     availableSpots = availableSpots.replace("\n", " <br> ")
 
+    timestamp = datetime.today().strftime('%d-%b %H:%M:%S')
+
     # Render HTML with count variable
-    return render_template("index.html", logs = availableSpots)
+    return render_template("index.html", logs = availableSpots, timestamp = timestamp)
 
 if __name__ == "__main__":
     app.run()
